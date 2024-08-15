931a4b08ebb2ec48aef1120bebf76199fdf44386
Leandro José Borges <ljborges76@gmail.com>
2024-08-15 02:28:56 +0000

    Recado para quem amo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Teste</h1>
+    return <h1>Esse recado vai para o amor para as minhas vidas: Você torna meu dias muito mais iluminados e coloridos minha Oxum</h1>
 }
 
 export default Home;
