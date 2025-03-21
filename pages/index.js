0770a2dae07d7a97e5027ba61542c2ad25ed9aad
Leandro José Borges <ljborges76@gmail.com>
2025-03-21 18:29:35 +0000

    add `/api/v1/status` endpoint

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Esse recado vai para o amor para as minhas vidas: Você torna meu dias
-      muito mais iluminados e coloridos minha Oxum <3
+      muito mais iluminados e coloridos minha Oxum 💖
     </h1>
   );
 }
