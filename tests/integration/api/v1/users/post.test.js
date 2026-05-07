ef80a722a5b07af8c07ae3d1e610bb30d3ea8ed0
Leandro José Borges <ljborges76@gmail.com>
2026-05-07 19:12:20 +0000

    feat: add `features` column to `users` table

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -32,6 +32,7 @@ describe("POST to /api/v1/users", () => {
         id: responseBody.id,
         username: "landro",
         email: "ljborgesdev@gmail.com",
+        features: [],
         password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
