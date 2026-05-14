6a4d115d24ca3620cb448769d804048d38cd7409
Leandro José Borges <ljborges76@gmail.com>
2026-05-14 20:55:27 +0000

    feat: apply `authorization.filterOutput()` to all endpoints

--- tests/integration/api/v1/users/[username]/get.test.js
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -10,7 +10,7 @@ beforeAll(async () => {
 describe("GET to /api/v1/users/[username]", () => {
   describe("Anonymous user", () => {
     test("With exact case match", async () => {
-      const createdUser = await orchestrator.createUser({
+      await orchestrator.createUser({
         username: "ExactCase",
       });
 
@@ -25,9 +25,7 @@ describe("GET to /api/v1/users/[username]", () => {
       expect(responseBody).toEqual({
         id: responseBody.id,
         username: "ExactCase",
-        email: createdUser.email,
         features: ["read:activation_token"],
-        password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -38,7 +36,7 @@ describe("GET to /api/v1/users/[username]", () => {
     });
 
     test("With exact case mismatch", async () => {
-      const createdUser = await orchestrator.createUser({
+      await orchestrator.createUser({
         username: "MismatchCase",
       });
 
@@ -53,9 +51,7 @@ describe("GET to /api/v1/users/[username]", () => {
       expect(responseBody).toEqual({
         id: responseBody.id,
         username: "MismatchCase",
-        email: createdUser.email,
         features: ["read:activation_token"],
-        password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
